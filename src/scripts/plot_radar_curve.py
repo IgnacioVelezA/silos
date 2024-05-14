dcd8b1fbefa8a0302126af486892691976faad32
IgnacioVelezA <ignacio.velez@ing.uchile.cl>
2024-05-14 15:13:38 -0400

    curve analizer

--- src/scripts/plot_radar_curve.py
+++ src/scripts/plot_radar_curve.py
@@ -12,12 +12,12 @@ def curve_analizer(curve):
     std = round(np.std(curve),2)
     mean = round(np.mean(curve),2)
     max_index = np.argmax(curve)
-    distance = distanceFinder.distanceFinder(curve, 15, 0, 30)
+    distance = distanceFinder.distanceFinder(curve, thr, 0, 30)
     return distance, mean, std
 
 #////END: curve_analizer =====================================================================
 
-def animate(i, xs, ys):
+def animate(i, xs, ys, thr):
     """
     This function is called periodically from FuncAnimation
     """
@@ -28,7 +28,7 @@ def animate(i, xs, ys):
 
     ys = np.array(curve)
     max_power = max(ys)
-    distance, mean, std = curve_analizer(ys)
+    distance, mean, std = curve_analizer(ys, thr)
 
     # Draw x and y lists
     ax.clear()
@@ -52,10 +52,12 @@ if __name__ == '__main__':
     parser.add_argument('arduino_usb_port', help='arduino usb port for serial comm, tipically /dev/ttyUSB1')
     parser.add_argument('-min', '--MINDISTANCE', default = 0, type = int)
     parser.add_argument('-max', '--MAXDISTANCE', default = 30, type = int)
+    parser.add_argument('-thr', '--THRESHOLD', default = 0, type = int)
     # Create figure for plotting
     args = parser.parse_args()
     MINDISTANCE = args.MINDISTANCE
     MAXDISTANCE = args.MAXDISTANCE
+    THRESHOLD = args.THRESHOLD
     xs = np.array(np.arange(start=MINDISTANCE, stop=MAXDISTANCE, step=MAXDISTANCE/128))
 
     fig = plt.figure()
@@ -65,7 +67,7 @@ if __name__ == '__main__':
     serial_radar.open_serial(args.arduino_usb_port)
     print(serial_radar.is_serial_open())
     # Set up plot to call animate() function periodically
-    ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=(800))
+    ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys, THRESHOLD), interval=(800))
     plt.show()
 
     serial_radar.close_serial()
