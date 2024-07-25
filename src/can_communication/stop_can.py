830948a44824e979bb7c4186db65f446799d0f30
IgnacioVelezA <ignacio.velez@ing.uchile.cl>
2024-07-25 11:30:57 -0400

    delete of not essencial scripts

--- src/can_communication/stop_can.py
+++
@@ -1,7 +0,0 @@
-import os
-import can
-from src.config.logging import logging
-
-logger = logging.get_logger(logging.MAIN_LOGGER)
-os.system('sudo ifconfig can0 down')
-logger.info('can0 shut down')
