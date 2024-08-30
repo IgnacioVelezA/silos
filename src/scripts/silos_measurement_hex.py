0c5d29c5e6fd2765d816064899836bdd3691ba62
IgnacioVelezA <ignacio.velez@ing.uchile.cl>
2024-08-30 11:44:43 -0400

    debugging

--- src/scripts/silos_measurement_hex.py
+++ src/scripts/silos_measurement_hex.py
@@ -1,5 +1,4 @@
 # This is the main script for silos measurement. Consists of 13 different points.
-
 from src.radar_communication import serial_radar
 from src.stepper_motor import serial_arduino
 from src.stepper_motor import stepper_motor
@@ -8,7 +7,7 @@ import time
 import math
 
 from datetime import datetime, timedelta, date
-import pickle as pkl
+import csv
 import argparse
 from src.trajectory import hex_trajectory
 
@@ -19,7 +18,6 @@ LOWEST_EL_ANGLE = 30    # Angle between zenith and lowest el point to measure
 
 measured_distances = []
 real_trajectory = []
-
 
 def point_and_measure(angles, measure_wait_time):
     az_motor.move(angles[0])
@@ -36,32 +34,24 @@ def point_and_measure(angles, measure_wait_time):
 
     curve = serial_radar.get_curve()
 
-    print("Measured distance at AZ = " + str(angles[0])
-        +" and EL = " + str(angles[1]))
+    print("Measured distance at AZ = " + str(angles[0]) + " and EL = " + str(angles[1]))
     
-    return measure_time, curve     #distance, curve)
-
+    return measure_time, curve
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='moves the radar to its 13 '+
-                                                 'positions and logs the info')
+    parser = argparse.ArgumentParser(description='moves the radar to its 13 positions and logs the info')
     
     parser.add_argument('filename', help='name of the file')
-    parser.add_argument('arduino_usb_port', help='arduino usb port for serial comm, tipically /dev/ttyUSB1')
+    parser.add_argument('arduino_usb_port', help='arduino usb port for serial comm, typically /dev/ttyUSB1')
     parser.add_argument('-r0','--radio_0', help='Radars HPBW')
-    parser.add_argument('-rmax','--radio_max', help='Radio of circular surface to map')
-    parser.add_argument('radar_usb_port', help='radar usb port for serial comm, tipically /dev/ttyUSB0')
-
-    #parser.add_argument('-nt','--number_of_trajectories', help ='number of trajectories to execute \n'+
-    #                                                   '1 by default, 0 means infinite')
+    parser.add_argument('-rmax','--radio_max', help='Radius of circular surface to map')
+    parser.add_argument('radar_usb_port', help='radar usb port for serial comm, typically /dev/ttyUSB0')
     parser.add_argument('-ar','--angle_repetition', help ='number of repetitions per angle, 1 by default')
-
     parser.add_argument('-rwait','--radar_wait_time', type = int, default = 10, help ='radar wait time, in seconds, before taking the curve')
-
     
     args = parser.parse_args()
 
-     # Defining points to measure
+    # Defining points to measure
     try:
         RADIO_BEAM = float(args.radio_0)
     except TypeError:
@@ -82,52 +72,27 @@ if __name__ == '__main__':
 
     traj = hex_trajectory.hex_trajectory(RADIO_BEAM, RADIO_SURFACE)
 
-
     time.sleep(1)
     todays_date = date.today()      
 
-    # file name ----------------------------------------------------------
+    # file name
     nameSD = 'measurements/'
-    #nameUSB = '/media/silos/15FC-A7CE/measurements/'
-    
-    if args.filename[-4:] == '.pkl':
-        suffix = args.filename[:-4]+ '_'+ str(todays_date) + '.pkl'
-        nameSD = nameSD + suffix
-        #nameUSB = nameUSB + suffix
+    if args.filename[-4:] == '.csv':
+        suffix = args.filename[:-4] + '_' + str(todays_date) + '.csv'
     else:
-        suffix = args.filename+ '_' + str(todays_date) + '.pkl'
-        nameSD = nameSD + suffix
-        #nameUSB = nameUSB + suffix
+        suffix = args.filename + '_' + str(todays_date) + '.csv'
     
-    # opening files ------------------------------------------------------
-    try :
-        # if files exist, just append data to it
-        fileSD = open(nameSD,'rb')
-        fileSD.close()
-        # fileUSB = open(nameUSB, 'rb')
-        # fileUSB.close()
-
-    except FileNotFoundError:
-        #if file does not exist initialize it
-            #trayectoria harcodeada
-        #traj = [(0, 0), (90, 5.0), (30.0, 5.0), (-30.0, 5.0), (-90, 5.0), (30.0, -10.0)]
-        #traj =[(0, 0), (90, 5.0), (30.0, 5.0), (-30.0, 5.0), (-90, 5.0), (30.0, -10.0), (-30.0, -10.0), (90, 10.0), (61.12, 8.5652), (30.0, 10.0), (1.12, 8.5652), (-30.0, 10.0), (-58.88, 8.5652), (-90, 10.0), (61.12, -13.5653), (30.0, -15.0), (1.12, -13.5652), (-30.0, -15.0), (-58.88, -13.5652), (90, 15.0), (71.69, 13.1667), (50.4, 12.9789), (30.0, 15.0), (11.69, 13.1667), (-9.6, 12.9789), (-30.0, 15.0), (-48.31, 13.1667), (-69.6, 12.9789), (-90, 15.0), (71.69, -18.1667), (50.4, -17.9789), (30.0, -20.0), (11.69, -18.1667), (-9.6, -17.9789), (-30.0, -20.0), (-48.31, -18.1667), (-69.6, -17.9789), (90, 20.0), (76.7, 17.9823), (61.12, 17.1304), (45.21, 17.6143), (30.0, 20.0), (16.7, 17.9823), (1.12, 17.1304), (-14.79, 17.6143), (-30.0, 20.0), (-43.3, 17.9823), (-58.88, 17.1304), (-74.79, 17.6143), (-90, 20.0), (76.7, -22.9823), (61.12, -22.1304), (45.21, -22.6144), (30.0, -25.0), (16.7, -22.9823), (1.12, -22.1304), (-14.79, -22.6144), (-30.0, -25.0), (-43.3, -22.9823), (-58.88, -22.1304), (-74.79, -22.6143), (90, 25.0), (79.58, 22.8771), (67.53, 21.6437), (54.66, 21.4538), (42.19, 22.3339), (30.0, 25.0), (19.58, 22.8771), (7.53, 21.6438), (-5.34, 21.4538), (-17.81, 22.3339), (-30.0, 25.0), (-40.42, 22.8771), (-52.47, 21.6437), (-65.34, 21.4538), (-77.81, 22.3339), (-90, 25.0), (79.58, -27.8771), (67.53, -26.6438), (54.66, -26.4538), (42.19, -27.3339), (30.0, -30.0), (19.58, -27.8771), (7.53, -26.6438), (-5.34, -26.4538), (-17.81, -27.3339), (-30.0, -30.0), (-40.42, -27.8771), (-52.47, -26.6437), (-65.34, -26.4538), (-77.81, -27.3339)]    
+    nameSD = nameSD + suffix
     
-        config_dict = {'params': (RADIO_BEAM, RADIO_SURFACE),
-                       'traj' : traj}
-
-        fileSD = open(nameSD,'wb')
-        pkl.dump(config_dict,fileSD)
-        fileSD.close()
-
-        # fileUSB = open(nameUSB, 'wb')
-        # pkl.dump(config_dict,fileUSB)
-        # fileUSB.close()
+    # Initialize CSV file with headers
+    with open(nameSD, mode='w', newline='') as fileSD:
+        writer = csv.writer(fileSD)
+        writer.writerow(['Measure Time', 'Azimuth', 'Elevation', 'Curve'])
 
     # Initializing radar serial comm
     serial_radar.open_serial(args.radar_usb_port)
     if serial_radar.is_serial_open():
-        print("Serial radar port open succesfully")
+        print("Serial radar port open successfully")
     else:
         print("Could not open serial radar port, FATAL ERROR")
 
@@ -135,44 +100,39 @@ if __name__ == '__main__':
     GPIO.cleanup()
     serial_arduino.open_serial(args.arduino_usb_port)
     if serial_arduino.is_serial_open():
-        print("Serial arduino port open succesfully")
+        print("Serial arduino port open successfully")
     else:
         print("Could not open arduino radar port, FATAL ERROR")
 
-    az_motor = stepper_motor.stepper_motor(id = 1,speed=0, max_speed=400, acceleration=600)
-    az_motor.add_limit_switch(12,-90) #12
-    az_motor.add_limit_switch(22,92) #24
+    az_motor = stepper_motor.stepper_motor(id=1, speed=0, max_speed=400, acceleration=600)
+    az_motor.add_limit_switch(12, -90)
+    az_motor.add_limit_switch(22, 92)
     time.sleep(1)
 
-    el_motor = stepper_motor.stepper_motor(id = 2, speed=0, max_speed=400, acceleration=600)
-    el_motor.add_limit_switch(18,-44) #18
-    el_motor.add_limit_switch(23,39) #23
+    el_motor = stepper_motor.stepper_motor(id=2, speed=0, max_speed=400, acceleration=600)
+    el_motor.add_limit_switch(18, -44)
+    el_motor.add_limit_switch(23, 39)
     time.sleep(1)
 
     # Initializing motors position using limit switches
-
-    az_motor.initialization(dir=1,speed=0, max_speed=400, acceleration=600)
+    az_motor.initialization(dir=1, speed=0, max_speed=400, acceleration=600)
     while not az_motor.is_initialized:
         time.sleep(MOTOR_STATUS_POLLING_TIME)
     time.sleep(0.1)
     az_motor.move(0)
 
-    el_motor.initialization(dir=1,speed=0, max_speed=400, acceleration=600) 
+    el_motor.initialization(dir=1, speed=0, max_speed=400, acceleration=600) 
     while not el_motor.is_initialized:
         time.sleep(MOTOR_STATUS_POLLING_TIME)
     time.sleep(0.1)
     el_motor.move(0)
     
-    
-    #az_motor.offset_angle_encoder = az_motor.read_encoder() #por ahora queda en 0 en la posición del ls reached
     print("Both motors are initialized")
     
     data_counter = 0
     
     # Pointing and measuring the corresponding angles
     ltraj = len(traj)
-
-    #fileUSB = open(nameUSB,'ab')
 
     measured_curves = [] 
     i = 0
@@ -181,10 +141,14 @@ if __name__ == '__main__':
     while True:
         try:
             print(f'//////--Point {i} out of {ltraj}--//////')
-            curve_repetition_n = point_and_measure(traj[i],radar_measure_wait_time)
+            measure_time, curve = point_and_measure(traj[i], radar_measure_wait_time)
 
-            measured_curves.append(curve_repetition_n) 
-
+            with open(nameSD, mode='a', newline='') as fileSD:
+                writer = csv.writer(fileSD)
+                writer.writerow([measure_time, traj[i][0], traj[i][1], curve])
+            
+            measured_curves.append((measure_time, traj[i], curve))
+            
         except Exception as error:
             # handle the exception
             print("An exception occurred:", error)
@@ -195,22 +159,22 @@ if __name__ == '__main__':
                 az_real_position = az_motor.read_encoder()
                 break
             except:
-                print('An error ocurred while reading encoders')
+                print('An error occurred while reading encoders')
                 time.sleep(0.1)
                 if attempt_i == max_attempts - 1:
                     print(f'Max attempts done, using nominal position for point {i} in az')
-                    az_real_position = 5600#traj[i][0]
+                    az_real_position = traj[i][0]
 
         for attempt_i in range(max_attempts):
             try:
                 el_real_position = el_motor.read_encoder()
                 break
             except:
-                print('An error ocurred while reading encoders')
+                print('An error occurred while reading encoders')
                 time.sleep(0.1)
                 if attempt_i == max_attempts - 1:
-                    print(f'Max attempts done, using nominal position for point {i} in az')
-                    el_real_position = 5601#traj[i][1]
+                    print(f'Max attempts done, using nominal position for point {i} in el')
+                    el_real_position = traj[i][1]
                         
         real_position = (az_real_position, el_real_position)
         print(real_position)
@@ -221,18 +185,14 @@ if __name__ == '__main__':
         
         i += 1
 
-    fileSD = open(nameSD,'ab')
-    pkl.dump(real_trajectory, fileSD)
-    pkl.dump(measured_curves, fileSD)
-    fileSD.close()
-    # pkl.dump(measured_curves, fileUSB)
-    # fileUSB.close()
+    # Save final trajectory
+    with open(nameSD, mode='a', newline='') as fileSD:
+        writer = csv.writer(fileSD)
+        writer.writerow(["Real Trajectory"] + real_trajectory)
 
     # Done, returning motors to 0 position
-    #-------------------------------------------------------------
     az_motor.move(0)
     el_motor.move(0)
-    #-------------------------------------------------------------
 
     # Printing results
     print(measured_curves)
